9987d31a42ce75ff4d552820334c660967c1f650
Alexander <acepetrov96@gmail.com>
2021-03-04 16:03:44 -0600

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SI-Frontend
+
+This is the Javascript frontend code for the "Ideahub" application. To begin, once the repo is cloned and navigated into, open the index.html file on your browser of choice, though I recommend Google Chrome. To do this on Windows, run `explorer.exe index.html`.
+
+Additionally you will need to clone and follow the README.md for the backend of this application, as you will need it running appropriately, as this application is not yet hosted. 
+The link is: https://github.com/AcePrincipal/Stakeholders-Ideas
+
+Once you have both the backend and frontend operating, navigate to the new tab that has been opened by running this apps index.html file, and notice there is a title, and images displayed beneath it. Choose an organization based off the images you see, then click on it to add your valuable idea in the comment section. You will be brought to a show page, which then will display an option to add an idea, then post it. Enjoy!
--- src/business.js
+++ src/business.js
@@ -2,33 +2,33 @@ const baseURL = "http://localhost:3000/"
 const businessesList = document.querySelector("#businesses-list")
 const businessShow = document.querySelector("#business-show")
 
-class business {
+class Business {
     static all = []
     constructor({id, name, img_url, b_url}){
         this.id = id
         this.name = name
         this.img_url = img_url
         this.b_url = b_url 
-        business.all.push(this)
+        Business.all.push(this)
     }
 
-    static fetchbusinesss(){
-        fetch(baseURL + "businesss")
+    static fetchBusinesses(){
+        fetch(baseURL + "businesses")
         .then(res => res.json())
         .then(businessData => {
             businessData.forEach(business => {
-                let organization = new business(business)
+                let organization = new Business(business)
                 organization.addToDom()
             })
         })
     }
 
     handleClick(e){
-        businesssList.style.display="none"
+        businessesList.style.display="none"
         businessShow.style.display=""
         this.addToShow()
         let businessId = parseInt(e.target.id.split("-")[1])
-        Review.fetchReviews(businessId)
+        Idea.fetchIdeas(businessId)
 
     }
 
@@ -36,7 +36,7 @@ class business {
         let img = document.createElement('img');
         img.src = this.img_url
         img.id = `business-${this.id}`
-        businesssList.appendChild(img)
+        businessesList.appendChild(img)
         img.addEventListener('click',(e) => this.handleClick(e))
     }
 
@@ -47,7 +47,8 @@ class business {
         let img = document.createElement('img');
         img.src = this.img_url
         img.id = `business-${this.id}`
-        let burl = document.createElement('h6');
+        let burl = document.createElement('a');
+        burl.href = this.b_url
         burl.innerText = this.b_url
         div.appendChild(title)
         div.appendChild(img)
@@ -57,7 +58,7 @@ class business {
         input.type="hidden"
         input.value= this.id
         input.id="business_id"
-        reviewForm.prepend(input)
+        ideaForm.prepend(input)
     }
 
 }
